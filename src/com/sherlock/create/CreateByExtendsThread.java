091c4184e64d23babf8ef70e68bee3a3899699f5
fanyanggod <fanyanggod95@gmail.com>
2021-12-02 16:21:49 +0800

    feature: update get thread name method

--- src/com/sherlock/create/CreateByExtendsThread.java
+++ src/com/sherlock/create/CreateByExtendsThread.java
@@ -8,7 +8,7 @@ public class CreateByExtendsThread {
         @Override
         public void run(){
             for (int i = 0; i < 5; i++) {
-                System.out.println(getName()+":"+i);
+                System.out.println(Thread.currentThread().getName()+":"+i);
             }
         }
     }
